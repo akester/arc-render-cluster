04b24501d6f3347e17701c574212c0cd8a652dcd
Andrew Kester <akester@cox.net>
2013-06-01 17:44:39 -0500

    www: adding login error page

---
+++ www/public/error.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<!--
+Design by Free CSS Templates
+http://www.freecsstemplates.org
+Released for free under a Creative Commons Attribution 2.5 License
+
+Name       : GoodLife 
+Description: A two-column, fixed-width design with dark color scheme.
+Version    : 1.0
+Released   : 20121013
+
+-->
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta name="keywords" content="" />
+	<meta name="description" content="" />
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	<title>ARC Render Cluster - Home</title>
+	<link href='http://fonts.googleapis.com/css?family=Oswald:400,300' 
+		rel='stylesheet' type='text/css' />
+	<link href='http://fonts.googleapis.com/css?family=Abel|Satisfy' 
+		rel='stylesheet' type='text/css' />
+	<link href="default.css" rel="stylesheet" type="text/css" media="all" />
+	<!--[if IE 6]>
+		<link href="default_ie6.css" rel="stylesheet" type="text/css" />
+	<![endif]-->
+</head>
+<body>
+	<div id="header-wrapper">
+		<div id="header">
+			<div id="logo">
+				<h1><a href="#">ARC</a></h1>
+			</div>
+			<div id="menu">
+				<ul>
+					<li>
+						<a href="index.php" accesskey="1">Home</a>
+					</li>
+					<li><a href="#" accesskey="2">About</a></li>
+					<li><a href="#" accesskey="3">Premium</a></li>
+					<li><a href="#" accesskey="4">Stats</a></li>
+					<li><a href="arc/index.php" accesskey="5">Log In</a></li>
+				</ul>
+			</div>
+		</div>
+	</div>
+	<div id="footer-wrapper">
+		<div id="footer-content">
+			<div id="fbox2">
+				<h2><span>Error</span></h2>
+				<p>
+					An error occured, please check your username and password
+					and try again later.
+				</p>
+			</div>
+		</div>
+	</div>
+	<div id="footer">
+		<p>
+			Copyright (c) 2012 ARC Render Cluster.  Design by 
+			<a href="http://www.freecsstemplates.org/" rel="nofollow">
+			FreeCSSTemplates.org</a>.  This design is released under the Creative
+			Commons Attribution 2.5 License.
+		</p>
+		<p>
+			Usage of ARC is subject to our Acceotable Use Policy.  Please consider
+			supporting this project by donating.
+		</p>
+	</div>
+</body>
+</html>
