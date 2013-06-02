ef5533d4910392f517f7cf9cd181cf7a9aed0907
Andrew Kester <akester@cox.net>
2013-06-01 20:27:33 -0500

    shed: adding config parsing function

---
+++ shed/src/shed.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import ConfigParser
+import errno
+import sys
+
+class shed ():
+    # System Specific Settings
+    configFileLocation= 'cfg/shed.conf'
+
+    def parseConfig(self):
+        # Read the configuration file
+        try:
+            config = ConfigParser.SafeConfigParser()
+            config.read(self.configFileLocation)
+        except ConfigParser.Error as e:
+            sys.stdout.write('Error parsing configuration.  Returned: {0}\n'
+                             .format(e))
+            sys.stdout.flush()
+            exit(-errno.EIO)
+
+        # Build the file into a dict that we can use
+        out = {}
+        for section in config.sections():
+            out[section] = {}
+            for option in config.options(section):
+                out[section][option] = config.get(section, option)
+
+        return out
