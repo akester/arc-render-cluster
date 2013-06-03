39539377da4bf4c03d78b45812288ee66eb271f8
Andrew Kester <akester@cox.net>
2013-06-02 21:30:22 -0500

    shed: adding unittest for ssh exec functions

--- shed/tests/main.py
+++ shed/tests/main.py
@@ -4,3 +4,15 @@ import sys
 sys.path.append('/home/andrew/projects/arc-render-cluster/shed/src')
 
 import shed
+import unittest
+
+class shed_SSHFunctions(unittest.TestCase):
+    def setUp(self):
+        self.s = shed.shed()
+        
+    def testSSHConnection(self):
+        self.s.sshCommand('render-01', 'uname -a')
+        
+# Begin Testing
+if __name__ == '__main__':
+    unittest.main()
