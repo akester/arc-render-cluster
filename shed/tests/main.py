3d397051d1869c673a94a5a9dc6cfee28211cadb
Andrew Kester <akester@cox.net>
2013-06-02 21:39:12 -0500

    shed: adding tests for main library functions

--- shed/tests/main.py
+++ shed/tests/main.py
@@ -13,6 +13,14 @@ class shed_SSHFunctions(unittest.TestCase):
     def testSSHConnection(self):
         self.s.sshCommand('render-01', 'uname -a')
         
+class shed_MainFunctions(unittest.TestCase):
+    def setUp(self):
+        self.s = shed.shed()
+    
+    def testLibrary(self):
+        self.assertIsInstance(self.s, shed.shed)
+        
+        
 # Begin Testing
 if __name__ == '__main__':
     unittest.main()
