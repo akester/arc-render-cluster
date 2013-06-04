18edd2ef3065eee0364e913ced99c6542c7e5a12
Andrew Kester <akester@cox.net>
2013-06-04 12:02:10 -0500

    shed: adding file to submit blender jobs to torque
    This marks a bit of a shift in thinking, rather than create a new
    scheduling program, we will rely on torque to do the sheduling and
    assignment of jobs to hosts.  This script takes a blender file and
    some options and sends the jobs to a cluster.

---
+++ shed/src/blender.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# ARC Cluster Blender Job Submission
+# Takes a blender file with some params
+# and submits the job to the cluster
+
+# Copyright 2013 Andrew Kester
+# Released under GNU-GPL
+
+##############################################
+
+set -e
+echo "Counting Frames..."
+# Load the vars
+infile=$1
+startf=$2
+endf=$3
+priority=$4
+
+# Set default priority
+if [ "$priority" = "" ]; then
+	priority=0
+fi
+
+totalframes=`expr $endf - $startf + 1`
+
+echo "Submitting jobs to cluster..."
+x=$startf
+while [ $x -le $endf ]; do
+	echo "Submitting frame $x"
+	#Build the submission script
+	cat <<EOS | qsub -
+#!/bin/bash
+#PBS -N blender-render-frame
+#PBS -l nodes=1:ppn=1
+
+blender -b $infile -x 1 -o //out -F JPEG -s $x -e $x -a
+EOS
+	x=`expr $x + 1`
+done
